73d4086fe05dd06f43cf169cd9fa1cd9366147fa
Ruan de Necker <ruandn@gmail.com>
2020-08-17 15:42:02 +0200

    Basic login with CORS

--- server/authenticator.js
+++ server/authenticator.js
@@ -1,9 +1,25 @@
-module.exports = function (request, response, next) {
-  var passcode = 'super-secret';
-  console.log(request.headers["Authorization"])
-  if (request.query.passcode === passcode) {
-    next();
-  } else {
-    response.send(401);
-  }
+const jwt = require("jsonwebtoken")
+
+module.exports = function (req, res, next) {
+  // var passcode = 'super-secret';
+  // console.log(request.headers["Authorization"])
+  // if (request.query.passcode === passcode) {
+  //   next();
+  // } else {
+  //   response.send(401);
+  // }
+
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401) // if there isn't any token
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err)
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+    console.log("This is the request user", req.user)
+    next()
+  })
+
 }
--- server/server.js
+++ server/server.js
@@ -1,18 +1,32 @@
 var path = require('path')
+require('dotenv').config()
 var express = require('express')
 var app = express()
 var port = 8000
+var cors = require('cors')
 var authenticator = require('./authenticator');
 var logger = require('./logger')
 var data = require('./data')
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+const jwt = require("jsonwebtoken")
+
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 var urlpath = path.join(__dirname, '../frontend/build/')
+// app.use(cors(corsOptions))
 //1.
 app.use(logger)
+app.use(cors(corsOptions))
 //2.
 app.use(express.static(urlpath))
 //3.
-app.use(authenticator);
+app.use(cookieParser())
+app.use(bodyParser.json())
+// app.use(authenticator);
 //4.
 app.param('name', function (request, response, next) {
   request.lowerName = request.params.name.toLowerCase();
@@ -34,6 +48,22 @@ app.get('/api/categories/:name/exercises', function (request, response) {
 
 app.get('/home', (req, res) => {
   res.redirect(301, '/')
+})
+
+app.post('/api/login', (req, res) => {
+  var loginDetails = req.body
+  console.log(loginDetails)
+  // Validate that the user exists in the database/datafile
+  // Decrypt password with bcrypt
+
+  // The regester rout will encrypt the password and the post into database
+  const token = jwt.sign({ "name": "Ruan", "id": "12334554" }, process.env.ACCESS_TOKEN_SECRET)
+  res.cookie("token", token)
+  res.json({ token: token })
+})
+
+app.post('/api/protected', authenticator, (req, res) => {
+  res.json(req.user)
 })
 
 app.listen(port, () => {
